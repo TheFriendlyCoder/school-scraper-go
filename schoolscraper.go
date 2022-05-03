1d72162f2761e0a7e13628e526ecc3876b3901b6
Kevin Phillips <thefriendlycoder@gmail.com>
2022-05-02 21:58:50 -0300

    crude working prototype

--- schoolscraper.go
+++ schoolscraper.go
@@ -1,5 +1,77 @@
 package schoolscraper
 
-func ScrapeSchools(html, schoolname string) string {
-	return "fubar2"
+import (
+	"fmt"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type school struct {
+	district   string
+	name       string
+	openstatus string
+	busstatus  string
+	message    string
+}
+
+func ScrapeSchools(body string) []school {
+	doc, err := html.Parse(strings.NewReader(body))
+	if err != nil {
+		panic(err)
+	}
+
+	var retval []school
+
+	var parseSchoolData func(*html.Node)
+	parseSchoolData = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "tr" {
+			var rowData [5]string
+			var counter = 0
+			var header = false
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				if c.Data != "td" {
+					continue
+				}
+
+				for d := c.FirstChild; d != nil; d = d.NextSibling {
+					for _, attr := range c.Attr {
+						if attr.Key == "class" && attr.Val == "tblH0" {
+							header = true
+						}
+					}
+					if header {
+						continue
+					}
+					rowData[counter] = d.Data
+				}
+				if header {
+					continue
+				}
+				counter++
+			}
+			if header {
+				return
+			}
+			if counter != 5 {
+				panic("Failed to parse school data")
+			}
+			temp := school{
+				district:   rowData[0],
+				name:       rowData[1],
+				openstatus: rowData[2],
+				busstatus:  rowData[3],
+				message:    rowData[4],
+			}
+
+			retval = append(retval, temp)
+			fmt.Println(temp)
+
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			parseSchoolData(c)
+		}
+	}
+	parseSchoolData(doc)
+	return retval
 }
---
+++ schoolscraper_test.go
@@ -0,0 +1,24 @@
+package schoolscraper
+
+import (
+	"os"
+	"testing"
+)
+
+func TestParser(t *testing.T) {
+	dat, err := os.ReadFile("sample.html")
+	if err != nil {
+		panic(err)
+	}
+
+	schools := ScrapeSchools(string(dat))
+
+	expected := "BAIE-SAINTE-ANNE"
+	if schools[0].district != expected {
+		t.Errorf("First district should be %v: %v", expected, schools[0].district)
+	}
+	expected_count := 37
+	if len(schools) != expected_count {
+		t.Errorf("Expected: %v Actual: %v", expected_count, len(schools))
+	}
+}
